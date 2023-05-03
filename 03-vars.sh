3a88494bcfc2f784843b5a3945637b99aa8fdad0
Parvateesam <parvateesam.boyina@gmail.com>
2023-05-03 10:51:07 +0530

    Variables example - Command Substitution

--- 03-vars.sh
+++ 03-vars.sh
@@ -5,3 +5,9 @@ echo "Hello, Good Morning, Today date is ${DATE}"
 #Command Substitution
 DATE=$(date)
 echo "Hello, Good Morning, Today Date is ${DATE}"
+
+
+#Arthimetic Substitution
+ADD=$((2+3))
+echo Addition of 2+3 = ${ADD}
+echo "Addition of 2+3 = ${ADD}"
